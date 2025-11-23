582d1b75a2c6e04e17b70a82a0651047b7450898
feedthehungersevasangha-creator <feedthehungersevasangha@gmail.com>
2025-11-23 18:45:36 +0530

    Update Donourentity.java

--- src/main/java/com/komal/template_backend/model/Donourentity.java
+++ src/main/java/com/komal/template_backend/model/Donourentity.java
@@ -1,28 +1,26 @@
-
-// ----------------------------------------------------------------------------------------
-// package com.komal.template_backend.model;
-
-// import org.springframework.data.annotation.Id;
-// import org.springframework.data.mongodb.core.mapping.Document;
-
-// import java.time.LocalDateTime;
-
-// @Document(collection = "Donations")
-// public class Donourentity {
+//package com.komal.template_backend.model;
+//
+//import org.springframework.data.annotation.Id;
+//import org.springframework.data.mongodb.core.mapping.Document;
+//
+//import java.time.LocalDateTime;
+//
+//@Document(collection = "Donations")
+//public class Donourentity {
 //    @Id
 //        private String id;
-
+//
 //        private String firstName;
 //        private String lastName;
 //        private String email;
 //        private String mobile;
 //        private String dob;
-
+//
 //        private String idType;
 //        private String uniqueId;  // 🔐 encrypted
 //    private String uniqueIdHash;   // HMAC for search
 //        private String address;
-
+//
 //        private String frequency;
 //        private double amount;
 //        private String paymentMode;
@@ -32,140 +30,140 @@
 //    private String ifscHash;       // HMAC//
 //        private String accountNumber; //
 //    private String accountNumberHash; // HMAC
-
+//
 //        private LocalDateTime donationDate = LocalDateTime.now();
-
-
+//
+//
 //    public String getId() {
 //        return id;
 //    }
-
+//
 //    public void setId(String id) {
 //        this.id = id;
 //    }
-
+//
 //    public String getFirstName() {
 //        return firstName;
 //    }
-
+//
 //    public void setFirstName(String firstName) {
 //        this.firstName = firstName;
 //    }
-
+//
 //    public String getLastName() {
 //        return lastName;
 //    }
-
+//
 //    public void setLastName(String lastName) {
 //        this.lastName = lastName;
 //    }
-
+//
 //    public String getEmail() {
 //        return email;
 //    }
-
+//
 //    public void setEmail(String email) {
 //        this.email = email;
 //    }
-
+//
 //    public String getMobile() {
 //        return mobile;
 //    }
-
+//
 //    public void setMobile(String mobile) {
 //        this.mobile = mobile;
 //    }
-
+//
 //    public String getDob() {
 //        return dob;
 //    }
-
+//
 //    public void setDob(String dob) {
 //        this.dob = dob;
 //    }
-
+//
 //    public String getIdType() {
 //        return idType;
 //    }
-
+//
 //    public void setIdType(String idType) {
 //        this.idType = idType;
 //    }
-
+//
 //    public String getUniqueId() {
 //        return uniqueId;
 //    }
-
+//
 //    public void setUniqueId(String uniqueId) {
 //        this.uniqueId = uniqueId;
 //    }
-
+//
 //    public String getAddress() {
 //        return address;
 //    }
-
+//
 //    public void setAddress(String address) {
 //        this.address = address;
 //    }
-
+//
 //    public String getFrequency() {
 //        return frequency;
 //    }
-
+//
 //    public void setFrequency(String frequency) {
 //        this.frequency = frequency;
 //    }
-
+//
 //    public double getAmount() {
 //        return amount;
 //    }
-
+//
 //    public void setAmount(double amount) {
 //        this.amount = amount;
 //    }
-
+//
 //    public String getPaymentMode() {
 //        return paymentMode;
 //    }
-
+//
 //    public void setPaymentMode(String paymentMode) {
 //        this.paymentMode = paymentMode;
 //    }
-
+//
 //    public String getIfsc() {
 //        return ifsc;
 //    }
-
+//
 //    public void setIfsc(String ifsc) {
 //        this.ifsc = ifsc;
 //    }
-
+//
 //    public String getBankName() {
 //        return bankName;
 //    }
-
+//
 //    public void setBankName(String bankName) {
 //        this.bankName = bankName;
 //    }
-
+//
 //    public String getAccountNumber() {
 //        return accountNumber;
 //    }
-
+//
 //    public void setAccountNumber(String accountNumber) {
 //        this.accountNumber = accountNumber;
 //    }
-
+//
 //    public LocalDateTime getDonationDate() {
 //        return donationDate;
 //    }
-
+//
 //    public void setDonationDate(LocalDateTime donationDate) {
 //        this.donationDate = donationDate;
 //    }
-// }
-
-
+//}
+//
+//
 package com.komal.template_backend.model;
 
 import org.springframework.data.annotation.Id;
@@ -572,3 +570,4 @@ public class Donourentity {
 
 
 }
+
