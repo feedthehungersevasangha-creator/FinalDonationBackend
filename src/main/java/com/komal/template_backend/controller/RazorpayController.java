1ad318516c4b817160328296d3007ef9e47208e4
feedthehungersevasangha-creator <feedthehungersevasangha@gmail.com>
2025-12-05 01:04:10 +0530

    Update RazorpayController.java

--- src/main/java/com/komal/template_backend/controller/RazorpayController.java
+++ src/main/java/com/komal/template_backend/controller/RazorpayController.java
@@ -1617,7 +1617,7 @@ public ResponseEntity<?> createSubscription(@RequestBody Map<String, Object> req
         subscriptionRequest.put("plan_id", plan.get("id"));
         subscriptionRequest.put("customer_notify", 1);
       System.out.println("Subscription yeasrs"+subscriptionYears);
-        subscriptionRequest.put("total_count", subscriptionYears );
+        subscriptionRequest.put("total_count", subscriptionYears * 12 );
         // ONLY the ₹1 auth addon here — do NOT add starterAmount as addon
         subscriptionRequest.put("addons", List.of(authAddon));
         subscriptionRequest.put("notes", notes);
@@ -1899,6 +1899,7 @@ public ResponseEntity<?> createSubscription(@RequestBody Map<String, Object> req
         }
     }
 }
+
 
 
 
